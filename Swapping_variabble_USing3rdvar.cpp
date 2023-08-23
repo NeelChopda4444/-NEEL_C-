3902d11c9d8ab0cca7273e06f197da5cb989043c
Neel Chopda <neelchopda21@gmail.com>
2023-08-23 13:36:48 +0530

    cpp program

--- Swapping_variabble_USing3rdvar.cpp
+++ Swapping_variabble_USing3rdvar.cpp
@@ -8,12 +8,12 @@ int main()
     int a, b,c;
 
     
-    cout << "Enter the value of a: ";
+    cout << "Enter the value of a:"; 
     cin >> a;
-    cout << "Enter the value of b: ";
+    cout << "Enter the value of b:";
     cin >> b;
 
-      cout << "Before swapping, a = " << a << " and b = " << b;
+      cout << "Before swapping, a =" << a << " and b =" << b;
 
    
     c = a;
