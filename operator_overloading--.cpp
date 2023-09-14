0a3a2a4ac4aba31b737f90daa488bc5e6dfee8f9
Neel Chopda <neelchopda21@gmail.com>
2023-09-14 09:19:23 +0530

    cpp program

---
+++ operator_overloading--.cpp
@@ -0,0 +1,38 @@
+// operator overloading:
+#include <iostream>
+using namespace std;
+class Squre
+{
+    int length;
+    public:
+    void setData(int len)
+    {
+        length = len;
+      
+    }
+
+    Squre operator--(int)
+    {
+        Squre box;
+
+        box.length = length--;
+        return box;
+    }
+
+    int getData()
+    {
+            return length * length;
+    }
+
+};
+
+int main ()
+{
+        Squre box1, box2;
+
+        box1.setData(3);
+        cout << "Box 1 is: " << box1.getData() << endl;
+
+        box1--;
+        cout << "Box 2 is: " << box1.getData() << endl;
+}
